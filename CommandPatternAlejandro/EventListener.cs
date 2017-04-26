02677a58fcc8a5f1cb32a8fd6360596235ba1bed
Jose Fernandez <jose.fernandez@ppcuban.com>
2017-04-25 22:52:05 -0400

    new file added

---
+++ CommandPatternAlejandro/EventListener.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Event;
+
+namespace CommandPatternAlejandro
+{
+    public class EventListener :
+        IPostInsertEventListener,
+        IPostDeleteEventListener,
+        IPostUpdateEventListener,
+        IPostCollectionUpdateEventListener
+    {
+        public void OnPostDelete(PostDeleteEvent @event)
+        {
+            DispatchEvents(@event.Entity as AggregateRoot);
+        }
+
+        private void DispatchEvents(AggregateRoot aggregateRoot)
+        {
+            var id = aggregateRoot.Id;
+        }
+
+        public void OnPostInsert(PostInsertEvent @event)
+        {
+            DispatchEvents(@event.Entity as AggregateRoot);
+        }
+
+        public void OnPostUpdate(PostUpdateEvent @event)
+        {
+            DispatchEvents(@event.Entity as AggregateRoot);
+        }
+
+        public void OnPostUpdateCollection(PostCollectionUpdateEvent @event)
+        {
+            DispatchEvents(@event.AffectedOwnerOrNull as AggregateRoot);
+        }
+    }
+}
